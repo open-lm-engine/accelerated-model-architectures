0f3c9844f62ea7463521d81153ed1679c652e99a
Mayank Mishra <mayank31398@gmail.com>
2026-01-03 15:41:54 -0800

    add sphinx
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -78,5 +78,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "tests/*"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+# html_theme = "alabaster"
+html_theme = "sphinx-rtd-theme"
 html_static_path = ["_static"]
