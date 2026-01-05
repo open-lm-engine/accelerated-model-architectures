106a0e2b4b8231d93362ad00844bfe55a7da9dad
Mayank Mishra <mayank31398@gmail.com>
2026-01-04 19:43:40 -0800

    init
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/functional/short_convolution/__init__.py
+++ xma/functional/short_convolution/__init__.py
@@ -1,10 +1,15 @@
+# **************************************************
+# Copyright (c) 2025, Mayank Mishra
+# **************************************************
+
 import torch
 import torch.nn.functional as F
 
+from ...accelerator import KernelBackend
 from ...custom_op import CustomOp
 
 
-class _ShortConvolution1D(CustomOp):
+class _CausalShortConvolution1D(CustomOp):
     @staticmethod
     def forward_backward_torch(
         x: torch.Tensor, weight: torch.Tensor, bias: torch.Tensor | None, stride: int, padding: int, groups: int
@@ -14,16 +19,46 @@ class _ShortConvolution1D(CustomOp):
             weight=weight,
             bias=bias,
             stride=stride,
-            padding=padding,
+            padding=kernel_size - 1,
             groups=groups,
         )
 
         return x
 
 
-def short_convolution_1D(
-    x: torch.Tensor, weight: torch.Tensor, bias: torch.Tensor | None, stride: int, padding: int, groups: int
+def causal_short_convolution_1D(
+    input: torch.Tensor,
+    weight: torch.Tensor,
+    bias: torch.Tensor | None,
+    stride: int,
+    padding: int,
+    groups: int,
+    cu_seqlens: torch.Tensor | None = None,
+    max_seqlen: torch.Tensor | int | None = None,
+    *,
+    kernel_backend: KernelBackend | None = None,
 ) -> tuple[torch.Tensor, torch.Tensor]:
-    x = _ShortConvolution1D.run(x=x, weight=weight, bias=bias, stride=stride, padding=padding, groups=groups)
+    """
+    1D short causal convolution
 
-    return x
+    :param input: input tensor
+    :type input: torch.Tensor
+    :param weight: weight tensor
+    :type weight: torch.Tensor
+    :param bias: bias tensor
+    :type bias: torch.Tensor | None
+    :param stride: convolution stride
+    :type stride: int
+    :param groups: convolution groups
+    :type groups: int
+    :return:
+    :rtype: tuple[Tensor, Tensor]
+    """
+
+    input = _CausalShortConvolution1D.run(
+        input=input, weight=weight, bias=bias, stride=stride, groups=groups, input_state=input_state
+    )
+
+    input_state = input[:, -1] if cu_seqlens is None else input[cu_seqlens[1:] - 1]
+
+    return input, input_state
