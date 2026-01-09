2ba08dd2c2cad4c724fe8e66a8311d57f8a3ff6f
Mayank Mishra <mayank31398@gmail.com>
2026-01-09 01:32:29 -0800

    I init
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/cute_dsl_utils/utils.py
+++ xma/cute_dsl_utils/utils.py
@@ -10,6 +10,19 @@ from cutlass.cute.runtime import from_dlpack
 from ..utils import get_alignment
 
 
+def get_fake_cute_tensor(x: torch.Tensor, divisibility: int = 1, leading_dim: int = -1) -> cute.Tensor:
+    if x.stride(leading_dim) != 1:
+        raise ValueError("leading_dim doesn't have stride 1")
+
+    if leading_dim < 0:
+        leading_dim = x.dim() + leading_dim
+
+    stride = tuple(1 if i == leading_dim else cute.sym_int64(divisibility=divisibility) for i in range(len(shape)))
+    tensor = cute.runtime.make_fake_tensor(dtype, shape, stride=stride, assumed_align=divisibility * dtype.width // 8)
+
+    return tensor
+
+
 def torch_tensor_to_cute_tensor(x: torch.Tensor, leading_dim: int) -> cute.Tensor:
     if leading_dim < 0:
         leading_dim += x.dim()
