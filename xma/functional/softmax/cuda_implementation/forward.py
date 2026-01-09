f9ef4f3ce554e06cbfeb9704f87d152454515e0c
Mayank Mishra <mayank31398@gmail.com>
2026-01-08 18:14:12 -0800

    I init
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/functional/softmax/cuda_implementation/forward.py
+++ xma/functional/softmax/cuda_implementation/forward.py
@@ -12,6 +12,39 @@ from ....custom_op import xma_op
 from ....cute_dsl_utils import torch_tensor_to_cute_tensor
 
 
+@cute.kernel
+def softmax_forward_cuda_kernel(
+    gX: cute.Tensor,
+    gY: cute.Tensor,
+    gID: cute.Tensor,
+    copy_atom: cute.CopyAtom,
+    tiled_copy: cute.TiledCopy,
+    shape: cute.Shape,
+) -> None:
+    BLOCK_ID, _, _ = cute.arch.block_idx()
+    THREAD_ID, _, _ = cute.arch.thread_idx()
+
+    block_coord = ((None, None), BLOCK_ID)
+
+
+@cute.jit
+def softmax_forward_cuda_jit(mX: cute.Tensor, mY: cute.Tensor) -> None:
+    BLOCK_SIZE = 128
+    vector_size = 128 // mX.element_type.width
+
+    thr_layout = cute.make_ordered_layout((BLOCK_SIZE >> LOG_WARP_SIZE, WARP_SIZE), order=(1, 0))
+    val_layout = cute.make_ordered_layout((1, vector_size), order=(1, 0))
+    tiler_mn, tv_layout = cute.make_layout_tv(thr_layout, val_layout)
+
+    mID = cute.make_identity_tensor(gX.shape)
+
+    gX = cute.zipped_divide(mX, tiler_mn)
+    gY = cute.zipped_divide(mY, tiler_mn)
+    gID = cute.zipped_divide(mID, tiler_mn)
+
+    softmax_forward_cuda_kernel(gX=gX, gY=gY, gID=gID, copy_atom=copy_atom, tiled_copy=tiled_copy, shape=mX.shape)
+
+
 @xma_op(mutates_args={"y"})
 def softmax_forward_cuda(x: torch.Tensor, y: torch.Tensor, logits_multiplier: float | None) -> None:
     x = torch_tensor_to_cute_tensor(x, leading_dim=-1)
