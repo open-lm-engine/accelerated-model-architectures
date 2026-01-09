6bf9e0a78b383189250861b8d0427684004a5770
Mayank Mishra <mayank31398@gmail.com>
2026-01-09 15:46:34 -0800

    I init
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/cute_dsl_utils/utils.py
+++ xma/cute_dsl_utils/utils.py
@@ -9,20 +9,19 @@ import cutlass.cute as cute
 from ..cute_dsl_utils import get_cute_dtype_from_torch_dtype
 
 
-def get_fake_cute_tensor(x: torch.Tensor, divisibility: int = 1, leading_dim: int = -1) -> cute.Tensor:
-    if x.stride(leading_dim) != 1:
+def get_fake_cute_tensor(
+    dtype: torch.dtype, shape: tuple[int], stride: tuple[int], divisibility: int = 1, leading_dim: int = -1
+) -> cute.Tensor:
+    if stride[leading_dim] != 1:
         raise ValueError("leading_dim doesn't have stride 1")
 
     if leading_dim < 0:
-        leading_dim = x.dim() + leading_dim
+        leading_dim = len(shape) + leading_dim
 
-    stride = tuple(1 if i == leading_dim else cute.sym_int64(divisibility=divisibility) for i in range(x.dim()))
+    stride = tuple(1 if i == leading_dim else cute.sym_int64(divisibility=divisibility) for i in range(len(shape)))
 
     tensor = cute.runtime.make_fake_tensor(
-        get_cute_dtype_from_torch_dtype(x.dtype),
-        x.size(),
-        stride=stride,
-        assumed_align=divisibility * x.dtype.itemsize // 8,
+        get_cute_dtype_from_torch_dtype(dtype), shape, stride=stride, assumed_align=divisibility * dtype.itemsize // 8
     )
 
     return tensor
