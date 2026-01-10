f8c5825f60c4b29663730caad47dcd3099fecf2a
Mayank Mishra <mayank31398@gmail.com>
2026-01-09 17:03:31 -0800

    I init
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/utils/tensor.py
+++ xma/utils/tensor.py
@@ -19,3 +19,16 @@ def empty_like_contiguous(x: torch.Tensor, dtype: torch.dtype | None = None) -> 
 
 def zeros_like_contiguous(x: torch.Tensor, dtype: torch.dtype | None = None) -> torch.Tensor:
     return torch.zeros_like(x, dtype=dtype, memory_format=torch.contiguous_format)
+
+
+def get_alignment(x: torch.Tensor) -> int:
+    x = x.data_ptr()
+
+    alignment = 4
+    for i in get_powers_of_2(4, 16):
+        if x % i != 0:
+            break
+        else:
+            alignment = i
+
+    return alignment
