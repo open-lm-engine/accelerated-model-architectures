320bc0ad3a3af7d4f988eda51937b2e1a16d5eeb
Mayank Mishra <mayank31398@gmail.com>
2026-02-17 16:26:09 -0800

    fix text
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/layers/hippo.py
+++ xma/layers/hippo.py
@@ -7,6 +7,8 @@ from __future__ import annotations
 import torch
 import torch.nn as nn
 
+from ..functional import hippo
+
 
 class HiPPO(nn.Module):
     def __init__(self, state_head_dim: int, measure: str = "legS") -> HiPPO:
@@ -15,10 +17,22 @@ class HiPPO(nn.Module):
 
         assert self.measure in ["legT", "lagT", "legS"]
 
+        self.A = nn.Parameter(torch.empty(self.state_head_dim, self.state_head_dim))
+        self.B = nn.Parameter(torch.empty(self.state_head_dim))
+
         self.reset_parameters()
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor: ...
+    def forward(
+        self,
+        input: torch.Tensor,
+        input_state: torch.Tensor | None = None,
+        cu_seqlens: torch.Tensor | None = None,
+        max_seqlen: int | None = None,
+    ) -> torch.Tensor:
+        return hippo(
+            input=input, A=self.A, B=self.B, input_state=input_state, cu_seqlens=cu_seqlens, max_seqlen=max_seqlen
+        )
 
     def reset_parameters(self) -> None:
-        self.A
-        self.B
+        self.A.normal_()
+        self.B.normal_()
