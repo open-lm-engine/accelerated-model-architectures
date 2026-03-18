d4e412ec72b88b650d256dacd1a4b2994d067462
Mayank Mishra <mayank31398@gmail.com>
2026-03-18 15:13:47 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/layers/causal_convolution.py
+++ xma/layers/causal_convolution.py
@@ -12,18 +12,22 @@ from ..functional import causal_convolution
 
 class CausalConvolution(nn.Conv1d):
     def forward(
-        self, x: torch.Tensor, input_state: torch.Tensor | None, attention_mask: torch.Tensor | None = None
+        self,
+        x: torch.Tensor,
+        input_state: torch.Tensor | None,
+        attention_mask: torch.Tensor | None = None,
+        *,
+        kernel_backend: KernelBackend | None = None,
     ) -> torch.Tensor:
         return causal_convolution(
             x=x,
             input_state=input_state,
             attention_mask=attention_mask,
             return_cache_state=cache_params is not None,
-            weight=self.conv1d.weight,
-            bias=self.conv1d.bias,
-            num_groups=self.num_groups,
+            weight=self.weight,
+            bias=self.bias,
+            groups=self.groups,
             stride=self.stride,
             activation_string=self.activation_string,
-            conv1d_padding=self.kernel_size - 1,
-            conv1d_stride=1,
+            kernel_backend=kernel_backend,
         )
