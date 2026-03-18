4e068891e5d557909e3de1267af518f7526c1135
Mayank Mishra <mayank31398@gmail.com>
2026-03-17 20:20:23 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/functional/causal_convolution/triton_implementation/forward.py
+++ xma/functional/causal_convolution/triton_implementation/forward.py
@@ -6,9 +6,9 @@ import torch
 import triton
 import triton.language as tl
 
-from ....accelerator import Accelerator
 from ....custom_op import xma_op
 from ....math import ceil_divide, get_next_power_of_2
+from ....triton_utils import silu
 
 
 @triton.jit
@@ -47,10 +47,9 @@ def causal_convolution_triton_kernel(
     MASK_K = BLOCK_K < K
 
     MASK_BSH = MASK_B[:, None, None] & MASK_S[None, :, None] & MASK_H[None, None, :]
+    MASK_HK = MASK_H[:, None] & MASK_K[None, :]
 
-    W = tl.load(
-        W_ptr + BLOCK_H[:, None] * W_stride[0] + BLOCK_K[None, :] * W_stride[2], mask=MASK_H[:, None] & MASK_K[None, :]
-    )
+    W = tl.load(W_ptr + BLOCK_H[:, None] * W_stride[0] + BLOCK_K[None, :] * W_stride[2], mask=MASK_HK)
 
     x = tl.load(
         x_ptr
@@ -63,6 +62,13 @@ def causal_convolution_triton_kernel(
     W = W.T
     W = W[None, :, :]
     y = W * x
+
+    if b_ptr is not None:
+        b = tl.load(b_ptr + BLOCK_H[:, None] * b_stride[0] + BLOCK_K[None, :] * b_stride[1], mask=MASK_HK)
+        y += b
+
+    if ACTIVATION == "swiglu" or ACTIVATION == "silu":
+        y = silu(y)
 
     tl.load(
         y_ptr
