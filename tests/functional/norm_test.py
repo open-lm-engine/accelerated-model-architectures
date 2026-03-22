444a51a9be895db5bcd7fed51273fe748a8b93f7
Mayank Mishra <mayank31398@gmail.com>
2026-03-22 13:38:41 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/functional/norm_test.py
+++ tests/functional/norm_test.py
@@ -6,48 +6,42 @@ from __future__ import annotations
 
 from typing import Callable
 
+import pytest
 import torch
-from parameterized import parameterized
 
 from xma import KernelBackend, norm, set_seed
 
-from ..test_commons import TestCommons
+from ..test_commons import assert_equal_tensors, get_random_duplicated_tensors, skip_if_incompatible_kernel_backend
 from .fused_residual_add_rmsnorm_test import _get_sizes
 
 
 _SEED = 42
 
 
-class NormTest(TestCommons):
-    @parameterized.expand(
-        TestCommons.make_args_matrix(
-            _get_sizes(),  # size
-            [KernelBackend.triton],  # KernelBackend
-            [torch.float32],  # dtype
-            [1, 2, 3, "inf"],  # p
-            [None, 0.9],  # multiplier
-            [norm, torch.compile(norm, fullgraph=True)],  # function
-        )
+@pytest.mark.parametrize("size", _get_sizes())
+@pytest.mark.parametrize("kernel_backend", [KernelBackend.triton])
+@pytest.mark.parametrize("dtype", [torch.float32])
+@pytest.mark.parametrize("p", [1, 2, 3, "inf"])
+@pytest.mark.parametrize("multiplier", [None, 0.9])
+@pytest.mark.parametrize("function", [norm, torch.compile(norm, fullgraph=True)])
+def test_p_norm(
+    size: tuple[int],
+    kernel_backend: KernelBackend,
+    dtype: torch.dtype,
+    p: int | str,
+    multiplier: float | None,
+    function: Callable,
+) -> None:
+    skip_if_incompatible_kernel_backend(kernel_backend)
+    device = kernel_backend.get_compatible_accelerator().get_current_device()
+
+    set_seed(_SEED)
+
+    x_kernel, x_expected = get_random_duplicated_tensors(size, device=device, dtype=dtype)
+
+    z_kernel = function(x=x_kernel, multiplier=multiplier, p=p)
+    z_expected = norm(x=x_expected, multiplier=multiplier, p=p, kernel_backend=KernelBackend.torch)
+
+    assert_equal_tensors(
+        z_kernel, z_expected, False, atol_float32=3.1e-3 if p == 3 else None, rtol_float32=0 if p == 3 else None
     )
-    def test_p_norm(
-        self,
-        size: tuple[int],
-        kernel_backend: KernelBackend,
-        dtype: torch.dtype,
-        p: int | str,
-        multiplier: float | None,
-        function: Callable,
-    ) -> None:
-        self.skip_if_incompatible_kernel_backend(kernel_backend)
-        device = kernel_backend.get_compatible_accelerator().get_current_device()
-
-        set_seed(_SEED)
-
-        x_kernel, x_expected = self.get_random_duplicated_tensors(size, device=device, dtype=dtype)
-
-        z_kernel = function(x=x_kernel, multiplier=multiplier, p=p)
-        z_expected = norm(x=x_expected, multiplier=multiplier, p=p, kernel_backend=KernelBackend.torch)
-
-        self.assert_equal_tensors(
-            z_kernel, z_expected, False, atol_float32=3.1e-3 if p == 3 else None, rtol_float32=0 if p == 3 else None
-        )
