c1d0c1d734203c742b3cf34a6aa95e2ed5404860
Mayank Mishra <mayank31398@gmail.com>
2026-03-23 16:34:57 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/functional/swiglu/mps_implementation/__init__.py
+++ xma/functional/swiglu/mps_implementation/__init__.py
@@ -9,12 +9,12 @@ from ....jit import mps_jit
 
 
 @xma_op(mutates_args={"y"})
-@cpp_jit(extensions=[".mm"])
+@mps_jit(extensions=[".mm"])
 def swiglu_forward_mps(g: torch.Tensor, u: torch.Tensor, y: torch.Tensor) -> None: ...
 
 
 @xma_op(mutates_args={"dg", "du"})
-@cpp_jit(extensions=[".mm"])
+@mps_jit(extensions=[".mm"])
 def swiglu_backward_mps(
     g: torch.Tensor, u: torch.Tensor, dy: torch.Tensor, dg: torch.Tensor, du: torch.Tensor
 ) -> None: ...
