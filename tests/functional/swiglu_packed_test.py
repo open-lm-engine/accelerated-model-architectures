f55324ad3aee936c1dedce329cd280f456a9a807
Mayank Mishra <mayank31398@gmail.com>
2026-03-22 18:06:37 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/functional/swiglu_packed_test.py
+++ tests/functional/swiglu_packed_test.py
@@ -9,19 +9,19 @@ import torch
 
 from xma import KernelBackend, ceil_divide, swiglu_packed
 
+from ..utils import assert_equal_tensors, get_random_duplicated_tensors, skip_if_incompatible_kernel_backend
 from .swiglu_test import _generate_args
 
 
 @pytest.mark.parametrize("size,dtype,kernel_backend,function", _generate_args())
 @torch._dynamo.config.patch(recompile_limit=1024)
 def test_swiglu_packed(
-    self,
     size: tuple[int],
     dtype: torch.dtype,
     kernel_backend: KernelBackend,
     function: Callable,
 ) -> None:
-    self.skip_if_incompatible_kernel_backend(kernel_backend)
+    skip_if_incompatible_kernel_backend(kernel_backend)
     device = kernel_backend.get_compatible_accelerator().get_current_device()
 
     multiple = 2
@@ -29,14 +29,14 @@ def test_swiglu_packed(
         multiple *= 16 // dtype.itemsize
     size = (size[0], ceil_divide(size[-1], multiple) * multiple)
 
-    x_kernel, x_expected = self.get_random_duplicated_tensors(size, device=device, dtype=dtype)
+    x_kernel, x_expected = get_random_duplicated_tensors(size, device=device, dtype=dtype)
 
     z_kernel = function(x_kernel, kernel_backend=kernel_backend)
     z_expected = swiglu_packed(x_expected, kernel_backend=KernelBackend.torch)
 
-    self.assert_equal_tensors(z_kernel, z_expected, False, atol_float32=4.9e-5, rtol_float32=0)
+    assert_equal_tensors(z_kernel, z_expected, False, atol_float32=4.9e-5, rtol_float32=0)
 
     z_kernel.mean().backward()
     z_expected.mean().backward()
 
-    self.assert_equal_tensors(x_kernel.grad, x_expected.grad, False, atol_float32=5e-6, rtol_float32=0)
+    assert_equal_tensors(x_kernel.grad, x_expected.grad, False, atol_float32=5e-6, rtol_float32=0)
