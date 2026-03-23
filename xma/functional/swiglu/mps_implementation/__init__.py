d966ad5ff7a3246c04d182b54b7326b671b89756
Mayank Mishra <mayank31398@gmail.com>
2026-03-23 16:31:48 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/functional/swiglu/mps_implementation/__init__.py
+++ xma/functional/swiglu/mps_implementation/__init__.py
@@ -2,33 +2,13 @@
 # Copyright (c) 2025, Mayank Mishra
 # **************************************************
 
-import os
-
 import torch
-from torch.utils.cpp_extension import load as load_cpp_extension
 
 from ....custom_op import xma_op
 from ....mps_utils import cpp_jit
 
 
 _module = None
-
-
-@torch.compiler.disable
-def _get_module():
-    global _module
-
-    if _module is None:
-        _dir = os.path.dirname(__file__)
-        _module = load_cpp_extension(
-            "xma_swiglu_mps",
-            sources=[os.path.join(_dir, "ops.mm")],
-            extra_cflags=["-O3", "-Wall", "-std=c++17"],
-            extra_ldflags=["-framework", "Metal", "-framework", "Foundation"],
-            verbose=True,
-        )
-
-    return _module
 
 
 @xma_op(mutates_args={"y"})
