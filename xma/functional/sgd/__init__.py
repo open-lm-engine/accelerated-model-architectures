f849ea6e20a838434a91d80bed0bdea971920a16
Mayank Mishra <mayank31398@gmail.com>
2026-03-31 00:17:05 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/functional/sgd/__init__.py
+++ xma/functional/sgd/__init__.py
@@ -3,6 +3,7 @@
 # **************************************************
 
 import torch
+from torch.optim.sgd import _multi_tensor_sgd, _single_tensor_sgd
 
 from ...accelerator import Accelerator, KernelBackend
 from .triton_implementation import sgd_horizontally_fused_triton, sgd_triton
@@ -23,11 +24,31 @@ def sgd(
     else:
         assert kernel_backend.verify_accelerator()
 
-    if horizontal_fusion:
-        sgd_horizontally_fused_triton(Ws=parameters, dWs=gradients, lr=lr, maximize=maximize)
-    else:
-        for W, dW in zip(parameters, gradients):
-            assert W.is_contiguous()
-            dW = dW.contiguous()
+    if kernel_backend == KernelBackend.cuda:
+        pass
+    elif kernel_backend == KernelBackend.triton:
+        if horizontal_fusion:
+            sgd_horizontally_fused_triton(Ws=parameters, dWs=gradients, lr=lr, maximize=maximize)
+        else:
+            for W, dW in zip(parameters, gradients):
+                assert W.is_contiguous()
+                dW = dW.contiguous()
 
-            sgd_triton(W=W, dW=dW, lr=lr, maximize=maximize)
+                sgd_triton(W=W, dW=dW, lr=lr, maximize=maximize)
+    elif kernel_backend == KernelBackend.torch:
+        (_multi_tensor_sgd if horizontal_fusion else _single_tensor_sgd)(
+            params=parameters,
+            grads=gradients,
+            momentum_buffer_list=None,
+            grad_scale=None,
+            found_inf=None,
+            weight_decay=0,
+            momentum=0,
+            lr=lr,
+            dampening=0,
+            nesterov=False,
+            maximize=maximize,
+            has_sparse_grad=False,
+        )
+    else:
+        raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
