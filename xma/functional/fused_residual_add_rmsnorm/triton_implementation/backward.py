89596769e13092bd8a00e2af278fbfd5cb7215fa
Mayank Mishra <mayank31398@gmail.com>
2026-04-08 23:36:34 -0700

    rename
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/functional/fused_residual_add_rmsnorm/triton_implementation/backward.py
+++ xma/functional/fused_residual_add_rmsnorm/triton_implementation/backward.py
@@ -71,7 +71,7 @@ def _fused_residual_add_rmsnorm_backward_triton_kernel(
             dyW *= W
             dW += dy * z
 
-        dx = (dyW - H_inv * r * z * tl.sum(dyW * xr, keep_dims=True)) * r
+        dx = (dyW - H_inv * z * tl.sum(dyW * z, keep_dims=True)) * r
 
         if dxr_ptr is not None:
             dx += tl.load(dxr_ptr + BLOCK_ID_B * dxr_stride[0] + BLOCK_H * dxr_stride[1], mask=MASK_H)
