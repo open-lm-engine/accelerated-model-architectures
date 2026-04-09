983e4f5f0a8943be383f21d64aada743c5c5eab0
Mayank Mishra <mayank31398@gmail.com>
2026-04-08 23:18:06 -0700

    Revert "rename"
    This reverts commit f57792d62dc25b1bfa234ff9dc80e27a365c7f06.

--- xma/functional/fused_residual_add_rmsnorm/__init__.py
+++ xma/functional/fused_residual_add_rmsnorm/__init__.py
@@ -131,6 +131,7 @@ class _FusedResidualAddRMSNorm(CustomOp):
         has_residual = r is not None
 
         ctx_save_for_backward(ctx, xr if has_residual else x, W, s)
+        ctx.eps = eps
         ctx.has_residual = has_residual
         ctx.multiplier = multiplier
 
@@ -143,7 +144,7 @@ class _FusedResidualAddRMSNorm(CustomOp):
         xr, W, s = ctx.saved_tensors
 
         dx, dr, dW = fused_residual_add_rmsnorm_backward(
-            xr=xr, W=W, s=s, dy=dy, dxr=dxr, has_residual=ctx.has_residual, multiplier=ctx.multiplier
+            xr=xr, W=W, s=s, dy=dy, dxr=dxr, has_residual=ctx.has_residual, multiplier=ctx.multiplier, eps=ctx.eps
         )
 
         return dx, dr, dW, *[None] * 5
