71eef802c41968506b60bc10ce982257fde0754e
Mayank Mishra <mayank31398@gmail.com>
2026-04-21 16:07:03 -0700

    packing
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/functional/bmm_test.py
+++ tests/functional/bmm_test.py
@@ -36,30 +36,29 @@ def test_bmm(
     device = kernel_backend.get_compatible_accelerator().get_current_device()
 
     set_seed(_SEED)
-    std = 0.02
 
     L = 7
     M = 417
 
-    A = (
-        torch.randn(
-            (L, size[0], M) if is_A_transposed else (L, M, size[0]),
-            device=device,
-            dtype=dtype,
-            requires_grad=False,
-        )
-        * std
+    A = torch.randint(
+        -8,
+        8,
+        (L, size[0], M) if is_A_transposed else (L, M, size[0]),
+        device=device,
+        dtype=dtype,
+        requires_grad=False,
     )
-    B = (
-        torch.randn(
-            (L, size[1], size[0]) if is_B_transposed else (L, *size),
-            device=device,
-            dtype=dtype,
-            requires_grad=False,
-        )
-        * std
+
+    B = torch.randint(
+        -8,
+        8,
+        (L, size[1], size[0]) if is_B_transposed else (L, *size),
+        device=device,
+        dtype=dtype,
+        requires_grad=False,
     )
-    C = torch.randn(L, M, size[1], device=device, dtype=dtype, requires_grad=False) * std if has_C else None
+
+    C = torch.randint(-8, 8, (L, M, size[1]), device=device, dtype=dtype, requires_grad=False) if has_C else None
 
     alpha = 0.3
     beta = 0.7 if has_C else 0
@@ -89,11 +88,5 @@ def test_bmm(
     assert_equal_tensors(
         output_kernel,
         output_expected,
-        False,
-        atol_float32=7e-5,
-        rtol_float32=1e-4,
-        atol_float16=1e-4,
-        rtol_float16=5e-3,
-        atol_bfloat16=1e-3,
-        rtol_bfloat16=7e-3,
+        True,
     )
