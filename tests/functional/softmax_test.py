4dcb66823bdf0c2f462d99764554bf705762f7b5
Mayank Mishra <mayank31398@gmail.com>
2026-04-21 19:17:40 -0700

    packing
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/functional/softmax_test.py
+++ tests/functional/softmax_test.py
@@ -47,4 +47,4 @@ def test_softmax(
     z_kernel.sum().backward()
     z_expected.sum().backward()
 
-    assert_equal_tensors(x_kernel.grad, x_expected.grad, False, atol_bfloat16=8.1e-5, rtol_bfloat16=0)
+    assert_equal_tensors(x_kernel.grad, x_expected.grad, False, atol_bfloat16=1.5e-4, rtol_bfloat16=0)
