6d6d3b7c9e046855b10533584d7ae2cf0f7ffb58
Mayank Mishra <mayank31398@gmail.com>
2026-04-21 20:13:41 -0700

    packing
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/functional/swiglu_test.py
+++ tests/functional/swiglu_test.py
@@ -75,7 +75,9 @@ def test_swiglu(size: tuple[int], dtype: torch.dtype, kernel_backend: KernelBack
     z_kernel = function(x_kernel, y_kernel, kernel_backend=kernel_backend)
     z_expected = swiglu(x_expected, y_expected, kernel_backend=KernelBackend.torch)
 
-    assert_equal_tensors(z_kernel, z_expected, False, atol_float32=7e-5, rtol_float32=0)
+    assert_equal_tensors(
+        z_kernel, z_expected, False, atol_float32=7e-5, rtol_float32=0, atol_float16=7.7e-5, rtol_float16=0
+    )
 
     z_kernel.mean().backward()
     z_expected.mean().backward()
