5e128c546867564826b3693a803cacf9a48760f3
Mayank Mishra <mayank31398@gmail.com>
2026-04-22 13:52:41 -0700

    init
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/layers/rnn_test.py
+++ tests/layers/rnn_test.py
@@ -161,8 +161,8 @@ def test_rnn(
             weight_kernel_grads[weight_name],
             weight_torch_grads[weight_name],
             False,
-            atol_float32=9e-3,
-            rtol_float32=0,
+            atol_float32=None if cu_seqlens is None else 4.9e-4,
+            rtol_float32=None if cu_seqlens is None else 0,
             atol_float16=1.3e-2,
             rtol_float16=0,
         )
