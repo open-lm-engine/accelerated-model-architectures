62b2d87fcc15bec9c8c7cbae774c609acb81b260
Mayank Mishra <mayank31398@gmail.com>
2026-05-19 13:40:00 -0700

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/functional/sequence_packing/cuda_implementation.py
+++ xma/functional/sequence_packing/cuda_implementation.py
@@ -23,6 +23,8 @@ class _PackUnpackSequenceCUDAKernel:
         self.pack = pack
         self.BLOCK_SIZE = BLOCK_SIZE
 
+    def _copy_array(self, copy_atom: cute.CopyAtom, tiled_copy: cute.TiledCopy) -> None: ...
+
     @cute.kernel
     def kernel(
         self,
@@ -56,21 +58,17 @@ class _PackUnpackSequenceCUDAKernel:
         vector_size = 128 // mX.element_type.width
 
         thr_layout = cute.make_ordered_layout((1, self.BLOCK_SIZE), order=(1, 0))
-        val_layout = cute.make_ordered_layout((self.B, vector_size), order=(1, 0))
+        val_layout = cute.make_ordered_layout((1, vector_size), order=(1, 0))
 
         mC = cute.make_identity_tensor(mX.shape)
-
-        gX = cute.zipped_divide(mX, tiler_mn)
-        gY = cute.zipped_divide(mY, tiler_mn)
-        gC = cute.zipped_divide(mC, tiler_mn)
 
         copy_atom = cute.make_copy_atom(cute.nvgpu.CopyUniversalOp(), gX.element_type)
         tiled_copy = cute.make_tiled_copy_tv(copy_atom, thr_layout, val_layout)
 
-        B = cute.size(gX, mode=[0])
-        S = cute.size(gX, mode=[1])
+        B = cute.size(mX, mode=[0])
+        S = cute.size(mX, mode=[1])
 
-        kernel = self.kernel(gX=gX, gY=gY, gC=gC, copy_atom=copy_atom, tiled_copy=tiled_copy, shape=mX.shape)
+        kernel = self.kernel(gX=mX, gY=mY, gC=mC, copy_atom=copy_atom, tiled_copy=tiled_copy, shape=mX.shape)
         kernel.launch(grid=(S, B, 1), block=(self.BLOCK_SIZE, 1, 1), stream=stream)
 
 
