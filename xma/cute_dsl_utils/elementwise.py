6a61f0a4ff5a6b297eed2d5293368648aa812393
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2026-07-01 15:17:43 -0700

    [REFACTOR] refactor kernels using template (#460)
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

---
+++ xma/cute_dsl_utils/elementwise.py
@@ -0,0 +1,233 @@
+# **************************************************
+# Copyright (c) 2026, Mayank Mishra
+# **************************************************
+
+from __future__ import annotations
+
+import cuda.bindings.driver as cuda
+import cutlass.cute as cute
+import torch
+from cutlass import Boolean, const_expr, range_constexpr
+
+from ..constants import LOG_WARP_SIZE, WARP_SIZE
+from .utils import get_fake_cute_tensor
+
+
+def get_compiled_elementwise_cuda_fn(cache: dict, key, kernel_class: type, example_tensors: tuple, div: int):
+    fn = cache.get(key)
+    if fn is None:
+        fake_tensors = [
+            (
+                None
+                if t is None
+                else get_fake_cute_tensor(
+                    dtype=t.dtype, shape=(cute.sym_int(), cute.sym_int(divisibility=div)), divisibility=div
+                )
+            )
+            for t in example_tensors
+        ]
+        stream = cuda.CUstream(torch.cuda.current_stream().cuda_stream)
+        fn = cute.compile(kernel_class(), *fake_tensors, stream, options="--enable-tvm-ffi")
+        cache[key] = fn
+    return fn
+
+
+@cute.jit
+def _load(
+    gX: cute.Tensor, rC: cute.Tensor, thr_copy, copy_atom: cute.CopyAtom, block_coord, is_within_boundary
+) -> cute.TensorSSA:
+    bX = gX[block_coord]
+    tX = thr_copy.partition_S(bX)
+    rX = cute.make_rmem_tensor_like(tX)
+
+    if is_within_boundary:
+        cute.copy(copy_atom, tX, rX)
+    else:
+        cute.copy(copy_atom, tX, rX, pred=rC)
+
+    return rX.load()
+
+
+@cute.jit
+def _store(
+    gY: cute.Tensor,
+    y: cute.TensorSSA,
+    rC: cute.Tensor,
+    thr_copy,
+    copy_atom: cute.CopyAtom,
+    block_coord,
+    is_within_boundary,
+) -> None:
+    bY = gY[block_coord]
+    tY = thr_copy.partition_D(bY)
+    rY = cute.make_rmem_tensor_like(tY)
+
+    rY.store(y)
+
+    if is_within_boundary:
+        cute.copy(copy_atom, rY, tY)
+    else:
+        cute.copy(copy_atom, rY, tY, pred=rC)
+
+
+class ElementwiseCUDAKernel:
+    BLOCK_SIZE: int = 128
+
+    def compute(self, *inputs):
+        raise NotImplementedError
+
+    @cute.kernel
+    def kernel(
+        self,
+        gX0: cute.Tensor,
+        gX1: cute.Tensor | None,
+        gX2: cute.Tensor | None,
+        gY0: cute.Tensor,
+        gY1: cute.Tensor | None,
+        gC: cute.Tensor,
+        copy_atom: cute.CopyAtom,
+        tiled_copy: cute.TiledCopy,
+        shape: cute.Shape,
+    ) -> None:
+        is_x1_none = const_expr(gX1 is None)
+        is_x2_none = const_expr(gX2 is None)
+        is_y1_none = const_expr(gY1 is None)
+
+        BLOCK_ID, _, _ = cute.arch.block_idx()
+        THREAD_ID, _, _ = cute.arch.thread_idx()
+
+        block_coord = ((None, None), BLOCK_ID)
+        bC = gC[block_coord]
+
+        thr_copy = tiled_copy.get_slice(THREAD_ID)
+        tC = thr_copy.partition_S(bC)
+
+        rC = cute.make_rmem_tensor(tC.shape, Boolean)
+        for i in range_constexpr(cute.size(rC)):
+            rC[i] = cute.elem_less(tC[i], shape)
+
+        is_within_boundary = cute.elem_less(tC[cute.size(tC) - 1], shape)
+
+        x0 = _load(
+            gX=gX0,
+            rC=rC,
+            thr_copy=thr_copy,
+            copy_atom=copy_atom,
+            block_coord=block_coord,
+            is_within_boundary=is_within_boundary,
+        )
+
+        if const_expr(not is_x1_none):
+            x1 = _load(
+                gX=gX1,
+                rC=rC,
+                thr_copy=thr_copy,
+                copy_atom=copy_atom,
+                block_coord=block_coord,
+                is_within_boundary=is_within_boundary,
+            )
+
+        if const_expr(not is_x2_none):
+            assert not is_x1_none
+
+            x2 = _load(
+                gX=gX2,
+                rC=rC,
+                thr_copy=thr_copy,
+                copy_atom=copy_atom,
+                block_coord=block_coord,
+                is_within_boundary=is_within_boundary,
+            )
+
+        if const_expr(is_x1_none):
+            y = self.compute(x0)
+        elif const_expr(is_x2_none):
+            y = self.compute(x0, x1)
+        else:
+            y = self.compute(x0, x1, x2)
+
+        if const_expr(is_y1_none):
+            y0 = y
+        else:
+            y0, y1 = y
+
+        _store(
+            gY=gY0,
+            y=y0,
+            rC=rC,
+            thr_copy=thr_copy,
+            copy_atom=copy_atom,
+            block_coord=block_coord,
+            is_within_boundary=is_within_boundary,
+        )
+
+        if const_expr(not is_y1_none):
+            _store(
+                gY=gY1,
+                y=y1,
+                rC=rC,
+                thr_copy=thr_copy,
+                copy_atom=copy_atom,
+                block_coord=block_coord,
+                is_within_boundary=is_within_boundary,
+            )
+
+    @cute.jit
+    def __call__(
+        self,
+        mX0: cute.Tensor,
+        mX1: cute.Tensor | None,
+        mX2: cute.Tensor | None,
+        mY0: cute.Tensor,
+        mY1: cute.Tensor | None,
+        stream: cuda.CUstream,
+    ) -> None:
+        vector_size = 128 // mX0.element_type.width
+
+        thr_layout = cute.make_ordered_layout((self.BLOCK_SIZE >> LOG_WARP_SIZE, WARP_SIZE), order=(1, 0))
+        val_layout = cute.make_ordered_layout((4, vector_size), order=(1, 0))
+        tiler_mn, tv_layout = cute.make_layout_tv(thr_layout, val_layout)
+
+        mC = cute.make_identity_tensor(mX0.shape)
+
+        is_x1_none = const_expr(mX1 is None)
+        is_x2_none = const_expr(mX2 is None)
+        is_y1_none = const_expr(mY1 is None)
+
+        gC = cute.zipped_divide(mC, tiler_mn)
+        gX0 = cute.zipped_divide(mX0, tiler_mn)
+
+        if const_expr(not is_x1_none):
+            gX1 = cute.zipped_divide(mX1, tiler_mn)
+        else:
+            gX1 = None
+
+        if const_expr(not is_x2_none):
+            assert not is_x1_none
+            gX2 = cute.zipped_divide(mX2, tiler_mn)
+        else:
+            gX2 = None
+
+        gY0 = cute.zipped_divide(mY0, tiler_mn)
+
+        if const_expr(not is_y1_none):
+            gY1 = cute.zipped_divide(mY1, tiler_mn)
+        else:
+            gY1 = None
+
+        copy_atom = cute.make_copy_atom(cute.nvgpu.CopyUniversalOp(), gX0.element_type)
+        tiled_copy = cute.make_tiled_copy_tv(copy_atom, thr_layout, val_layout)
+
+        NUM_BLOCKS = cute.size(gX0, mode=[1])
+
+        self.kernel(
+            gX0=gX0,
+            gX1=gX1,
+            gX2=gX2,
+            gY0=gY0,
+            gY1=gY1,
+            gC=gC,
+            copy_atom=copy_atom,
+            tiled_copy=tiled_copy,
+            shape=mX0.shape,
+        ).launch(grid=(NUM_BLOCKS, 1, 1), block=(self.BLOCK_SIZE, 1, 1), stream=stream)
--- xma/functional/swiglu/cuda_implementation/forward.py
+++ xma/functional/swiglu/cuda_implementation/forward.py
@@ -7,102 +7,20 @@ from __future__ import annotations
 import math
 
 import cuda.bindings.driver as cuda
-import cutlass.cute as cute
 import torch
-from cutlass import Boolean, Float32, range_constexpr
+from cutlass import Float32
 
-from ....constants import LOG_WARP_SIZE, WARP_SIZE
 from ....custom_op import xma_op
-from ....cute_dsl_utils import get_fake_cute_tensor, sigmoid
+from ....cute_dsl_utils import sigmoid
+from ....cute_dsl_utils.elementwise import ElementwiseCUDAKernel, get_compiled_elementwise_cuda_fn
 
 
-class SwiGLUForwardCUDAKernel:
-    def __init__(self, BLOCK_SIZE: int = 128) -> SwiGLUForwardCUDAKernel:
-        self.BLOCK_SIZE = BLOCK_SIZE
-
-    @cute.kernel
-    def kernel(
-        self,
-        gG: cute.Tensor,
-        gU: cute.Tensor,
-        gY: cute.Tensor,
-        gC: cute.Tensor,
-        copy_atom: cute.CopyAtom,
-        tiled_copy: cute.TiledCopy,
-        shape: cute.Shape,
-    ) -> None:
-        BLOCK_ID, _, _ = cute.arch.block_idx()
-        THREAD_ID, _, _ = cute.arch.thread_idx()
-
-        block_coord = ((None, None), BLOCK_ID)
-
-        bG = gG[block_coord]
-        bU = gU[block_coord]
-        bY = gY[block_coord]
-        bC = gC[block_coord]
-
-        thr_copy = tiled_copy.get_slice(THREAD_ID)
-
-        tG = thr_copy.partition_S(bG)
-        tU = thr_copy.partition_S(bU)
-        tY = thr_copy.partition_D(bY)
-        tC = thr_copy.partition_S(bC)
-
-        rG = cute.make_rmem_tensor_like(tG)
-        rU = cute.make_rmem_tensor_like(tU)
-        rY = cute.make_rmem_tensor_like(tY)
-
-        rC = cute.make_rmem_tensor(tC.shape, Boolean)
-        for i in range_constexpr(cute.size(rC)):
-            rC[i] = cute.elem_less(tC[i], shape)
-
-        is_within_boundary = cute.elem_less(tC[cute.size(tC) - 1], shape)
-
-        if is_within_boundary:
-            cute.copy(copy_atom, tG, rG)
-            cute.copy(copy_atom, tU, rU)
-        else:
-            cute.copy(copy_atom, tG, rG, pred=rC)
-            cute.copy(copy_atom, tU, rU, pred=rC)
-
-        g = rG.load()
-        u = rU.load()
-
+class SwiGLUForwardCUDAKernel(ElementwiseCUDAKernel):
+    def compute(self, g, u):
         dtype = g.dtype
         g = g.to(Float32)
         y = u * g * sigmoid(g)
-        y = y.to(dtype)
-
-        rY.store(y)
-
-        if is_within_boundary:
-            cute.copy(copy_atom, rY, tY)
-        else:
-            cute.copy(copy_atom, rY, tY, pred=rC)
-
-    @cute.jit
-    def __call__(self, mG: cute.Tensor, mU: cute.Tensor, mY: cute.Tensor, stream: cuda.CUstream) -> None:
-        vector_size = 128 // mG.element_type.width
-
-        thr_layout = cute.make_ordered_layout((self.BLOCK_SIZE >> LOG_WARP_SIZE, WARP_SIZE), order=(1, 0))
-        val_layout = cute.make_ordered_layout((4, vector_size), order=(1, 0))
-        tiler_mn, tv_layout = cute.make_layout_tv(thr_layout, val_layout)
-
-        mC = cute.make_identity_tensor(mG.shape)
-
-        gG = cute.zipped_divide(mG, tiler_mn)
-        gU = cute.zipped_divide(mU, tiler_mn)
-        gY = cute.zipped_divide(mY, tiler_mn)
-        gC = cute.zipped_divide(mC, tiler_mn)
-
-        copy_atom = cute.make_copy_atom(cute.nvgpu.CopyUniversalOp(), gG.element_type)
-        tiled_copy = cute.make_tiled_copy_tv(copy_atom, thr_layout, val_layout)
-
-        NUM_BLOCKS = cute.size(gG, mode=[1])
-
-        self.kernel(gG=gG, gU=gU, gY=gY, gC=gC, copy_atom=copy_atom, tiled_copy=tiled_copy, shape=mG.shape).launch(
-            grid=(NUM_BLOCKS, 1, 1), block=(self.BLOCK_SIZE, 1, 1), stream=stream
-        )
+        return y.to(dtype)
 
 
 _CACHE = {}
@@ -113,21 +31,6 @@ def _swiglu_forward_cuda(g: torch.Tensor, u: torch.Tensor, y: torch.Tensor) -> N
     N = g.size(1)
     div = math.gcd(16 // g.dtype.itemsize, N)
 
-    key = (g.dtype, div)
-    function = _CACHE.get(key, None)
-
     stream = cuda.CUstream(torch.cuda.current_stream().cuda_stream)
-
-    if function is None:
-        _g, _u, _y = [
-            get_fake_cute_tensor(
-                dtype=i.dtype, shape=(cute.sym_int(), cute.sym_int(divisibility=div)), divisibility=div
-            )
-            for i in (g, u, y)
-        ]
-
-        function = SwiGLUForwardCUDAKernel()
-        function = cute.compile(function, _g, _u, _y, stream, options="--enable-tvm-ffi")
-        _CACHE[key] = function
-
-    function(g, u, y, stream)
+    fn = get_compiled_elementwise_cuda_fn(_CACHE, (g.dtype, div), SwiGLUForwardCUDAKernel, (g, u, None, y, None), div)
+    fn(g, u, None, y, None, stream)
