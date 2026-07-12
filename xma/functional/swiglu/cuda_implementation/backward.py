f47230e149c5bf6b988f056a8768666c545634eb
Mayank Mishra <mayank31398@gmail.com>
2026-07-12 01:54:37 -0700

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/functional/swiglu/cuda_implementation/backward.py
+++ xma/functional/swiglu/cuda_implementation/backward.py
@@ -16,7 +16,23 @@ from ....custom_op import xma_op
 from ....cute_dsl_utils import ElementwiseCUDAKernel, get_compiled_elementwise_cuda_kernel, sigmoid
 
 
-class SwiGLUBackwardCUDAKernel(ElementwiseCUDAKernel):
+class _SwiGLUBackwardCUDAKernel(ElementwiseCUDAKernel):
+    def compute(self, xs: list[cute.TensorSSA]) -> list[cute.TensorSSA]:
+        g, u, dy = xs
+
+        dtype = g.dtype
+        g = g.to(Float32)
+
+        g_sigmoid = sigmoid(g)
+        g_silu = g * g_sigmoid
+
+        dg = dy * u * (g_sigmoid + g_silu * (1 - g_sigmoid))
+        du = dy * g_silu
+
+        return dg.to(dtype), du.to(dtype)
+
+
+class _SwiGLUBackwardPackedCUDAKernel(ElementwiseCUDAKernel):
     def compute(self, xs: list[cute.TensorSSA]) -> list[cute.TensorSSA]:
         g, u, dy = xs
 
@@ -44,10 +60,29 @@ def _swiglu_backward_cuda(
     kernel = get_compiled_elementwise_cuda_kernel(
         caller_op=_swiglu_backward_cuda,
         key=(g.dtype, div),
-        kernel_class=partial(SwiGLUBackwardCUDAKernel, BLOCK_SIZE=256),
+        kernel_class=partial(_SwiGLUBackwardCUDAKernel, BLOCK_SIZE=256),
         example_tensors_list=([g, u, dy], [dg, du]),
         div=div,
         stream=stream,
     )
 
     kernel([g, u, dy], [dg, du], stream)
+
+
+@xma_op(mutates_args={"dx"})
+def _swiglu_packed_backward_cuda(x: torch.Tensor, dy: torch.Tensor, dx: torch.Tensor) -> None:
+    N = x.size(1) >> 1
+    div = math.gcd(8 // x.dtype.itemsize, N)
+
+    stream = cuda.CUstream(torch.cuda.current_stream().cuda_stream)
+
+    kernel = get_compiled_elementwise_cuda_kernel(
+        caller_op=_swiglu_packed_backward_cuda,
+        key=(x.dtype, div),
+        kernel_class=partial(_SwiGLUBackwardPackedCUDAKernel, BLOCK_SIZE=256),
+        example_tensors_list=([dy], [x], [], [dx]),
+        div=div,
+        stream=stream,
+    )
+
+    kernel([dy], [x], [], [dx], stream)
