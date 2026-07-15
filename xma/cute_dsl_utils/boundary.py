acca181eba5e9edf5b9317cdb07efce721dba649
Mayank Mishra <mayank31398@gmail.com>
2026-07-15 11:53:44 -0700

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/cute_dsl_utils/boundary.py
+++ xma/cute_dsl_utils/boundary.py
@@ -14,9 +14,12 @@ def lane_boundary(gC: cute.Tensor, tiled_copy: cute.TiledCopy, block_coord, THRE
     tC = thr_copy.partition_S(bC)
 
     rC = cute.make_rmem_tensor(tC.shape, Boolean)
-    for i in range_constexpr(cute.size(rC)):
-        rC[i] = cute.elem_less(tC[i], shape)
-
     is_within_boundary = cute.elem_less(tC[cute.size(tC) - 1], shape)
+
+    if is_within_boundary:
+        pass
+    else:
+        for i in range_constexpr(cute.size(rC)):
+            rC[i] = cute.elem_less(tC[i], shape)
 
     return thr_copy, rC, is_within_boundary
