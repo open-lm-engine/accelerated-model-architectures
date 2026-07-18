e480748d39b0e491c74835f4d2a703602816ca79
Mayank Mishra <mayank31398@gmail.com>
2026-07-17 22:49:27 -0700

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/functional/swiglu/pallas_implementation/forward.py
+++ xma/functional/swiglu/pallas_implementation/forward.py
@@ -2,20 +2,23 @@
 # Copyright (c) 2026, Mayank Mishra
 # **************************************************
 
-import torch
-from torch_xla.experimental.custom_kernel import jax_import_guard, make_kernel_from_pallas
-
-from ....custom_op import xma_op
-from ....math import ceil_divide
+from ....utils import is_torch_xla_available
 
 
-jax_import_guard()
+if is_torch_xla_available():
+    import torch
+    from torch_xla.experimental.custom_kernel import jax_import_guard, make_kernel_from_pallas
+
+    jax_import_guard()
 
 import jax
 import jax.experimental.pallas as pl
 import jax.experimental.pallas.tpu as pltpu
 import jax.numpy as jnp
 from jax.nn import sigmoid
+
+from ....custom_op import xma_op
+from ....math import ceil_divide
 
 
 def _swiglu_forward_pallas_kernel(g_ref, u_ref, y_ref):
@@ -51,24 +54,24 @@ def _swiglu_forward_pallas_jit(g: jax.Array, u: jax.Array) -> jax.Array:
     return kernel(g, u)
 
 
-def _fake_function(g: torch.Tensor, u: torch.Tensor) -> torch.Tensor:
-    assert g.is_contiguous()
-    assert u.is_contiguous()
+if is_torch_xla_available():
 
-    return torch.empty_like(g)
+    def _fake_function(g: torch.Tensor, u: torch.Tensor) -> torch.Tensor:
+        assert g.is_contiguous()
+        assert u.is_contiguous()
 
+        return torch.empty_like(g)
 
-_CACHE = None
+    _CACHE = None
 
+    @xma_op(mutates_args={}, fake_func=_fake_function)
+    def _swiglu_forward_pallas(g: torch.Tensor, u: torch.Tensor) -> torch.Tensor:
+        assert g.is_contiguous()
+        assert u.is_contiguous()
 
-@xma_op(mutates_args={}, fake_func=_fake_function)
-def _swiglu_forward_pallas(g: torch.Tensor, u: torch.Tensor) -> torch.Tensor:
-    assert g.is_contiguous()
-    assert u.is_contiguous()
+        global _CACHE
 
-    global _CACHE
+        if _CACHE is None:
+            _CACHE = make_kernel_from_pallas(_swiglu_forward_pallas_jit, lambda g, u: [(g.shape, g.dtype)])
 
-    if _CACHE is None:
-        _CACHE = make_kernel_from_pallas(_swiglu_forward_pallas_jit, lambda g, u: [(g.shape, g.dtype)])
-
-    return _CACHE(g, u)
+        return _CACHE(g, u)
