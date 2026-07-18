767e30bc3741d0c1bc150198099a75a7cb4119d5
Mayank Mishra <mayank31398@gmail.com>
2026-07-18 09:54:44 -0700

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/functional/pack_sequence_test.py
+++ tests/functional/pack_sequence_test.py
@@ -6,10 +6,8 @@ from typing import Callable
 
 import pytest
 
-
-torch = pytest.importorskip("torch")
-
 from xma import KernelBackend, pack_sequence, unpack_sequence
+from xma.utils import is_torch_available
 
 from ..utils import (
     assert_equal_tensors,
@@ -30,95 +28,97 @@ def _get_problem_shapes(packed: bool) -> list[tuple[int]]:
     return sizes
 
 
-@torch_test
-@pytest.mark.parametrize("size", _get_problem_shapes(False))
-@pytest.mark.parametrize("cu_seqlens", [[0, 70, 170, 295, 393, 412, 515, 691]])
-@pytest.mark.parametrize("dtype", [torch.float32, torch.float16, torch.bfloat16])
-@pytest.mark.parametrize("padding_side", ["left", "right"])
-@pytest.mark.parametrize("kernel_backend", [KernelBackend.cuda, KernelBackend.triton])
-@pytest.mark.parametrize("function", [pack_sequence, torch.compile(pack_sequence, fullgraph=True)])
-@torch._dynamo.config.patch(recompile_limit=1024)
-def test_pack_sequence(
-    size: tuple[int],
-    cu_seqlens: list[int],
-    dtype: torch.dtype,
-    padding_side: str,
-    kernel_backend: KernelBackend,
-    function: Callable,
-) -> None:
-    skip_if_incompatible_kernel_backend(kernel_backend)
-    device = kernel_backend.get_compatible_accelerator().get_current_device()
+if is_torch_available():
+    import torch
 
-    x_kernel, x_expected = get_random_duplicated_tensors(size, device=device, dtype=dtype)
-    cu_seqlens = torch.tensor(cu_seqlens, device=device, dtype=torch.uint32)
+    @torch_test
+    @pytest.mark.parametrize("size", _get_problem_shapes(False))
+    @pytest.mark.parametrize("cu_seqlens", [[0, 70, 170, 295, 393, 412, 515, 691]])
+    @pytest.mark.parametrize("dtype", [torch.float32, torch.float16, torch.bfloat16])
+    @pytest.mark.parametrize("padding_side", ["left", "right"])
+    @pytest.mark.parametrize("kernel_backend", [KernelBackend.cuda, KernelBackend.triton])
+    @pytest.mark.parametrize("function", [pack_sequence, torch.compile(pack_sequence, fullgraph=True)])
+    @torch._dynamo.config.patch(recompile_limit=1024)
+    def test_pack_sequence(
+        size: tuple[int],
+        cu_seqlens: list[int],
+        dtype: torch.dtype,
+        padding_side: str,
+        kernel_backend: KernelBackend,
+        function: Callable,
+    ) -> None:
+        skip_if_incompatible_kernel_backend(kernel_backend)
+        device = kernel_backend.get_compatible_accelerator().get_current_device()
 
-    z_kernel = function(
-        [x_kernel],
-        cu_seqlens=cu_seqlens,
-        total_tokens=cu_seqlens[-1].item(),
-        padding_side=padding_side,
-        kernel_backend=kernel_backend,
-    )[0]
+        x_kernel, x_expected = get_random_duplicated_tensors(size, device=device, dtype=dtype)
+        cu_seqlens = torch.tensor(cu_seqlens, device=device, dtype=torch.uint32)
 
-    z_expected = pack_sequence(
-        [x_expected],
-        cu_seqlens=cu_seqlens.to(torch.int),
-        total_tokens=cu_seqlens[-1].item(),
-        padding_side=padding_side,
-        kernel_backend=KernelBackend.torch,
-    )[0]
+        z_kernel = function(
+            [x_kernel],
+            cu_seqlens=cu_seqlens,
+            total_tokens=cu_seqlens[-1].item(),
+            padding_side=padding_side,
+            kernel_backend=kernel_backend,
+        )[0]
 
-    z_expected.sum().backward()
-    z_kernel.sum().backward()
+        z_expected = pack_sequence(
+            [x_expected],
+            cu_seqlens=cu_seqlens.to(torch.int),
+            total_tokens=cu_seqlens[-1].item(),
+            padding_side=padding_side,
+            kernel_backend=KernelBackend.torch,
+        )[0]
 
-    assert_equal_tensors(z_kernel, z_expected, True)
-    assert_equal_tensors(x_kernel.grad, x_expected.grad, True)
+        z_expected.sum().backward()
+        z_kernel.sum().backward()
 
+        assert_equal_tensors(z_kernel, z_expected, True)
+        assert_equal_tensors(x_kernel.grad, x_expected.grad, True)
 
-@torch_test
-@pytest.mark.parametrize("size", _get_problem_shapes(True))
-@pytest.mark.parametrize("cu_seqlens", [[0, 70, 170, 295, 393, 412, 515, 691]])
-@pytest.mark.parametrize("sequence_length", [1000])
-@pytest.mark.parametrize("dtype", [torch.float32, torch.float16, torch.bfloat16])
-@pytest.mark.parametrize("padding_side", ["left", "right"])
-@pytest.mark.parametrize("kernel_backend", [KernelBackend.cuda, KernelBackend.triton])
-@pytest.mark.parametrize("function", [unpack_sequence, torch.compile(unpack_sequence, fullgraph=True)])
-@torch._dynamo.config.patch(recompile_limit=1024)
-def test_unpack_sequence(
-    size: tuple[int],
-    cu_seqlens: list[int],
-    sequence_length: tuple[int],
-    dtype: torch.dtype,
-    padding_side: str,
-    kernel_backend: KernelBackend,
-    function: Callable,
-) -> None:
-    skip_if_incompatible_kernel_backend(kernel_backend)
-    device = kernel_backend.get_compatible_accelerator().get_current_device()
+    @torch_test
+    @pytest.mark.parametrize("size", _get_problem_shapes(True))
+    @pytest.mark.parametrize("cu_seqlens", [[0, 70, 170, 295, 393, 412, 515, 691]])
+    @pytest.mark.parametrize("sequence_length", [1000])
+    @pytest.mark.parametrize("dtype", [torch.float32, torch.float16, torch.bfloat16])
+    @pytest.mark.parametrize("padding_side", ["left", "right"])
+    @pytest.mark.parametrize("kernel_backend", [KernelBackend.cuda, KernelBackend.triton])
+    @pytest.mark.parametrize("function", [unpack_sequence, torch.compile(unpack_sequence, fullgraph=True)])
+    @torch._dynamo.config.patch(recompile_limit=1024)
+    def test_unpack_sequence(
+        size: tuple[int],
+        cu_seqlens: list[int],
+        sequence_length: tuple[int],
+        dtype: torch.dtype,
+        padding_side: str,
+        kernel_backend: KernelBackend,
+        function: Callable,
+    ) -> None:
+        skip_if_incompatible_kernel_backend(kernel_backend)
+        device = kernel_backend.get_compatible_accelerator().get_current_device()
 
-    x_kernel, x_expected = get_random_duplicated_tensors(size, device=device, dtype=dtype)
-    cu_seqlens = torch.tensor(cu_seqlens, device=device, dtype=torch.uint32)
+        x_kernel, x_expected = get_random_duplicated_tensors(size, device=device, dtype=dtype)
+        cu_seqlens = torch.tensor(cu_seqlens, device=device, dtype=torch.uint32)
 
-    z_kernel = function(
-        [x_kernel],
-        cu_seqlens=cu_seqlens,
-        batch_size=cu_seqlens.size(0) - 1,
-        sequence_length=sequence_length,
-        padding_side=padding_side,
-        kernel_backend=kernel_backend,
-    )[0]
+        z_kernel = function(
+            [x_kernel],
+            cu_seqlens=cu_seqlens,
+            batch_size=cu_seqlens.size(0) - 1,
+            sequence_length=sequence_length,
+            padding_side=padding_side,
+            kernel_backend=kernel_backend,
+        )[0]
 
-    z_expected = unpack_sequence(
-        [x_expected],
-        cu_seqlens=cu_seqlens.to(torch.int),
-        batch_size=cu_seqlens.size(0) - 1,
-        sequence_length=sequence_length,
-        padding_side=padding_side,
-        kernel_backend=KernelBackend.torch,
-    )[0]
+        z_expected = unpack_sequence(
+            [x_expected],
+            cu_seqlens=cu_seqlens.to(torch.int),
+            batch_size=cu_seqlens.size(0) - 1,
+            sequence_length=sequence_length,
+            padding_side=padding_side,
+            kernel_backend=KernelBackend.torch,
+        )[0]
 
-    z_expected.sum().backward()
-    z_kernel.sum().backward()
+        z_expected.sum().backward()
+        z_kernel.sum().backward()
 
-    assert_equal_tensors(z_kernel, z_expected, True)
-    assert_equal_tensors(x_kernel.grad, x_expected.grad, True)
+        assert_equal_tensors(z_kernel, z_expected, True)
+        assert_equal_tensors(x_kernel.grad, x_expected.grad, True)
