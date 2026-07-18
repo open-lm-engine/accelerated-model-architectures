d38b8ff76979932d970d03185b559df25dc17864
Mayank Mishra <mayank31398@gmail.com>
2026-07-18 13:04:41 -0700

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,9 +15,9 @@ def pytest_configure(config) -> None:
     if not is_torch_available() or Accelerator.get_accelerator() not in [Accelerator.cuda, Accelerator.rocm]:
         return
 
-    gpu_count = Accelerator.device_count()
-    if gpu_count == 0:
+    num_accelerators = Accelerator.device_count()
+    if num_accelerators == 0:
         return
 
-    gpu_id = int(worker_id.replace("gw", "")) % gpu_count
+    gpu_id = int(worker_id.replace("gw", "")) % num_accelerators
     os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
